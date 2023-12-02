27cdb9bfae2ac5c3f52ce0a4fa79d24e07f6a0fc
dnyaneshwar2023 <dnyaneshwarw@sahaj.ai>
2023-12-02 12:40:30 +0530

    Add day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    val lines = readInput("input")
+
+    // Part 1
+    println(countScore(lines))
+
+    // Part 2
+    val digitList = mapOf<String, Int>(
+        Pair("one", 1),
+        Pair("1", 1),
+        Pair("two", 2),
+        Pair("2", 2),
+        Pair("three", 3),
+        Pair("3", 3),
+        Pair("four", 4),
+        Pair("4", 4),
+        Pair("five", 5),
+        Pair("5", 5),
+        Pair("six", 6),
+        Pair("6", 6),
+        Pair("seven", 7),
+        Pair("7", 7),
+        Pair("eight", 8),
+        Pair("8", 8),
+        Pair("nine", 9),
+        Pair("9", 9),
+    )
+
+    println(countScore(lines, digitList))
+}
+
+// Part 1
+private fun countScore(lines: List<String>): Int {
+    val answer = lines.sumOf { it ->
+        val firstDigit = it.first { it in '0'..'9' }.digitToInt()
+        val lastDigit = it.last { it in '0'..'9' }.digitToInt()
+        firstDigit * 10 + lastDigit
     }
+    return answer
+}
 
-    fun part2(input: List<String>): Int {
-        return input.size
+// Part 2
+private fun countScore(lines: List<String>, digits: Map<String, Int>): Int {
+
+    return lines.sumOf {
+        val firstDigit = it.findAnyOf(digits.keys)
+        val lastDigit = it.findLastAnyOf(digits.keys)
+
+        digits[firstDigit!!.second]!! * 10 + digits[lastDigit!!.second]!!
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
 }
